deb10620bfc80ddbd7abacb917c4dd3f2c455d72
Name Surname <dmitriy91pozdeev@mail.ru>
2023-10-16 11:29:03 +0300

    Added Test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxOfEquals() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {2, 5, 8, 4, 5, 3, 8, 6, 8, 4, 11};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
